24a8a04e68718490344b5d20af00bad8b924987c
firephinx <kevinleezhang@gmail.com>
2014-09-13 00:51:54 -0700

    Version 3.3

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ static void update_time() {
 static void main_window_load(Window *window) {
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 110, 144, 110));
-  s_text_layer = text_layer_create(GRect(10, 10, 144, 100));
+  s_text_layer = text_layer_create(GRect(10, 5, 127, 115));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_background_color(s_text_layer, GColorClear);
@@ -42,8 +42,9 @@ static void main_window_load(Window *window) {
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GAlignBottom);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_text_layer, "Hello World");
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text(s_text_layer, "Do not dismay - groovier ceilings shall accomplish.");
+  
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
