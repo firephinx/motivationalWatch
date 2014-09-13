9cc240b677e1275b37616b12511f7fd6d78a53a5
firephinx <kevinleezhang@gmail.com>
2014-09-12 23:19:46 -0700

    Version 2.0

--- src/main.c
+++ src/main.c
@@ -1,13 +1,61 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
+static GFont s_time_font;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
 
 static void main_window_load(Window *window) {
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 95, 144, 100));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
 
+  // Improve the layout to be more like a watchface
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Bauer_Bodoni_Bold_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GAlignBottom);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+}
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void window_load()
+{
+    window_stack_push(s_main_window, true);
+    update_time();
 }
 
 static void init() {
@@ -21,6 +69,9 @@ static void init() {
   });
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+    
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
